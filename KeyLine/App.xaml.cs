d860d957a65466adde7c1df4af97dac3d6aaae0a
Metehan Goksel Kurtulan <metehankurtulan@hotmail.com>
2026-06-05 02:38:36 +0300

    prevented 2 keylines running at the same time

--- KeyLine/App.xaml.cs
+++ KeyLine/App.xaml.cs
@@ -2,6 +2,7 @@ using System.Configuration;
 using System.Data;
 using System.Windows;
 using System.Windows.Controls;
+using System.Threading;
 using KeyLine.Interop;
 
 namespace KeyLine;
@@ -11,7 +12,10 @@ namespace KeyLine;
 /// </summary>
 public partial class App : Application
 {
+    private const string SingleInstanceMutexName = @"Local\KeyLine.App.SingleInstance";
     private const int GlobalToolTipInitialShowDelayMilliseconds = 350;
+
+    private Mutex? _singleInstanceMutex;
 
     static App()
     {
@@ -23,6 +27,36 @@ public partial class App : Application
     protected override void OnStartup(StartupEventArgs e)
     {
         NativeMethods.SetProcessDpiAwarenessContext(NativeMethods.DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+
+        _singleInstanceMutex = new Mutex(true, SingleInstanceMutexName, out var createdNew);
+        if (!createdNew)
+        {
+            _singleInstanceMutex.Dispose();
+            _singleInstanceMutex = null;
+            Shutdown();
+            return;
+        }
+
         base.OnStartup(e);
+
+        MainWindow = new MainWindow();
+        MainWindow.Show();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        try
+        {
+            _singleInstanceMutex?.ReleaseMutex();
+        }
+        catch (ApplicationException)
+        {
+            // The mutex was not owned; shutdown should continue normally.
+        }
+
+        _singleInstanceMutex?.Dispose();
+        _singleInstanceMutex = null;
+
+        base.OnExit(e);
     }
 }
